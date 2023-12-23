068e89b057450ff16fe0e0e3addf730b48de9e40
adamburley <52085927+adamburley@users.noreply.github.com>
2023-12-22 21:06:55 -0500

    Update Set-HuduPassword.ps1

--- HuduAPI/Public/Set-HuduPassword.ps1
+++ HuduAPI/Public/Set-HuduPassword.ps1
@@ -1,157 +1,90 @@
+# passing an invalid password_folder_id will result in the password being created but not visible. the password page will have a at the bottom a message like: " There are passwords hidden from view because of incorrect folder IDs. Fix Passwords [link]"
+# URL parameter is excluded from pipeline as passing the existing object back (as of 2.27) will corrupt any existing field value. see known issues.
+# password_folder_name is a read-only value
+# slug is a read-only value
+
 function Set-HuduPassword {
-    <#
-    .SYNOPSIS
-    Update a Password
-
-    .DESCRIPTION
-    Uses Hudu API to update a password
-
-    .PARAMETER Id
-    Id of the requested Password
-
-    .PARAMETER Name
-    Password name
-
-    .PARAMETER CompanyId
-    Id of requested company
-
-    .PARAMETER PasswordableType
-    Type of asset to associate with the password
-
-    .PARAMETER PasswordableId
-    Id of the asset to associate with the password
-
-    .PARAMETER InPortal
-    Display password in portal
-
-    .PARAMETER Password
-    Password
-
-    .PARAMETER OTPSecret
-    OTP secret
-
-    .PARAMETER URL
-    Url for the password
-
-    .PARAMETER Username
-    Username
-
-    .PARAMETER Description
-    Password description
-
-    .PARAMETER PasswordType
-    Password type
-
-    .PARAMETER PasswordFolderId
-    Id of requested password folder
-
-    .PARAMETER Slug
-    Url identifier
-
-    .EXAMPLE
-    Set-HuduPassword -Id 1 -CompanyId 1 -Password 'this_is_my_new_password'
-
-    #>
-    [CmdletBinding(SupportsShouldProcess)]
-    # This will silence the warning for variables with Password in their name.
+    [CmdletBinding(ShouldProcess,DefaultParameterSetName = 'Default')]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPlainTextForPassword', '')]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingUsernameAndPasswordParams', '')]
-    Param (
-        [Parameter(Mandatory = $true)]
-        [Int]$Id,
+    param(
+        [parameter(ValueFromPipelineByPropertyName, Mandatory)]
+        [int]$Id,
 
-        [String]$Name,
-
+        [parameter(ParameterSetName='Put',ValueFromPipelineByPropertyName, Mandatory)]
+        [parameter(ParameterSetName='Default',ValueFromPipelineByPropertyName)]
         [Alias('company_id')]
-        [Int]$CompanyId,
+        [int]$CompanyId,
 
-        [Alias('passwordable_type')]
-        [String]$PasswordableType = '',
+        [parameter(ValueFromPipelineByPropertyName)]
+        [string]$Name,
 
-        [Alias('passwordable_id')]
-        [int]$PasswordableId = '',
+        [parameter(ValueFromPipelineByPropertyName)]
+        [string]$Username,
 
+        [parameter(ParameterSetName='Put',ValueFromPipelineByPropertyName, Mandatory)]
+        [parameter(ParameterSetName='Default',ValueFromPipelineByPropertyName)]
+        [string]$Password,
+
+        [parameter(ValueFromPipelineByPropertyName)]
+        [Alias('otp_secret', 'TOTP')]
+        [string]$OTPKey,
+
+        [parameter()]
+        [string]$URL,
+
+        [parameter(ValueFromPipelineByPropertyName)]
+        [Alias('description')]
+        [string]$Notes,
+
+        [parameter(ValueFromPipelineByPropertyName)]
         [Alias('in_portal')]
-        [Bool]$InPortal = $false,
-        [String]$Password = '',
+        [bool]$InPortal = $false,
 
-        [Alias('otp_secret')]
-        [string]$OTPSecret = '',
+        [parameter(ValueFromPipelineByPropertyName)]
+        [Alias('passwordable_type')]
+        [ValidateSet('Asset', 'Website')]
+        [string]$ParentType,
 
-        [String]$URL = '',
+        [parameter(ValueFromPipelineByPropertyName)]
+        [Alias('passwordable_id')]
+        [int]$ParentId,
 
-        [String]$Username = '',
-
-        [String]$Description = '',
-
-        [Alias('password_type')]
-        [String]$PasswordType = '',
-
+        [parameter(ValueFromPipelineByPropertyName)]
         [Alias('password_folder_id')]
-        [int]$PasswordFolderId,
+        [int]$FolderId,
 
-        [string]$Slug
+        [switch]$Put
     )
+    process {
+        $payload = @{}
+        if ($Put) {
+            Write-Verbose "PUT specified."
+            throw 'notimplementedexception'
+            }
+        else {
+            # Fetching the existing layout first, so we can emulate a PATCH request.
+            $payload = Get-HuduPassword -Id $Id | Select-Object -ExcludeProperty id, slug, created_at, updated_at,url,password_folder_name | ConvertTo-Json | ConvertFrom-Json -AsHashtable # TODO REWRITE
+            #Write-Host ($password | ConvertTo-Json -Compress) -ForegroundColor Yellow
+            if (-not $payload) {
+                Write-Error "No password with ID [$Id] found." -ErrorAction Stop
+            }
+            
+        }
+        if ($CompanyId) { $payload.company_id = $CompanyId }
+        if ($Password) { $payload.password = $Password}
 
-    $Object = Get-HuduPasswords -Id $Id 
-    $AssetPassword = [ordered]@{asset_password = $Object }
-
-    if ($Name) {
-        $AssetPassword.asset_password | Add-Member -MemberType NoteProperty -Name name -Force -Value $Name
-        
-    }
-    
-    if ($CompanyId) {
-        $AssetPassword.asset_password | Add-Member -MemberType NoteProperty -Name company_id -Force -Value $CompanyId
-    }
-    
-    if ($Password) {
-        $AssetPassword.asset_password | Add-Member -MemberType NoteProperty -Name password -Force -Value $Password
-    }
-    
-    if ($InPortal) {
-        $AssetPassword.asset_password | Add-Member -MemberType NoteProperty -Name in_portal -Force -Value $InPortal
-    }
-    
-
-    if ($PasswordableType) {
-        $AssetPassword.asset_password | Add-Member -MemberType NoteProperty -Name passwordable_type -Force -Value $PasswordableType
-    }
-    if ($PasswordableId) {
-        $AssetPassword.asset_password | Add-Member -MemberType NoteProperty -Name passwordable_id -Force -Value $PasswordableId
-    }
-
-    if ($OTPSecret) {
-        $AssetPassword.asset_password | Add-Member -MemberType NoteProperty -Name otp_secret -Force -Value $OTPSecret
-    }
-
-    if ($URL) {
-        $AssetPassword.asset_password | Add-Member -MemberType NoteProperty -Name url -Force -Value $URL
-    }
-
-    if ($Username) {
-        $AssetPassword.asset_password | Add-Member -MemberType NoteProperty -Name username -Force -Value $Username
-    }
-
-    if ($Description) {
-        $AssetPassword.asset_password | Add-Member -MemberType NoteProperty -Name description -Force -Value $Description
-    }
-
-    if ($PasswordType) {
-        $AssetPassword.asset_password | Add-Member -MemberType NoteProperty -Name password_type -Force -Value $PasswordType
-    }
-
-    if ($PasswordFolderId) {
-        $AssetPassword.asset_password | Add-Member -MemberType NoteProperty -Name password_folder_id -Force -Value $PasswordFolderId
-    }
-
-    if ($Slug) {
-        $AssetPassword.asset_password | Add-Member -MemberType NoteProperty -Name slug -Force -Value $Slug
-    }
-
-    $JSON = $AssetPassword | ConvertTo-Json -Depth 10
-
-    if ($PSCmdlet.ShouldProcess($Id)) {
-        Invoke-HuduRequest -Method put -Resource "/api/v1/asset_passwords/$Id" -Body $JSON
+        if ($Name) { $payload.name = $Name }
+        if ($Username) { $payload.username = $Username }
+        if ($OTPKey) { $payload.otp_secret = $OTPKey }
+        if ($URL) { $payload.url = $URL }
+        if ($Notes) { $payload.description = $Notes }
+        if ($InPortal) { $payload.in_portal = $InPortal }
+        if ($ParentType) { $payload.passwordable_type = $ParentType }
+        if ($ParentId) { $payload.passwordable_id = $ParentId }
+        if ($FolderId) { $payload.password_folder_id = $FolderId }
+        if ($PSCmdlet.ShouldProcess($Id)) {
+            #Invoke-HuduRequest -Method put -Resource "/api/v1/asset_passwords/$Id" -Body $JSON
+        }
     }
 }
