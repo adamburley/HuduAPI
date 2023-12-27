f23e98afc3f1be890ada4e10da9d26afa4848992
adamburley <52085927+adamburley@users.noreply.github.com>
2023-12-27 15:25:15 -0500

    Create Find-ObjectIdByReference.ps1

---
+++ HuduAPI/Private/Find-ObjectIdByReference.ps1
@@ -0,0 +1,30 @@
+function Find-ObjectIdByReference {
+    [CmdletBinding(PositionalBinding)]
+    param(
+        [Parameter(ValueFromPipeline, Mandatory)]
+        [AllowNull()][AllowEmptyString()]
+        [object]$Reference,
+
+        [Parameter(Position = 0, Mandatory)]
+        [ValidateSet('Company', 'AssetLayout')]
+        [string]$Type
+    )
+    process {
+        if ($null -eq $Reference) { return $null }
+        $id = switch ($Reference.GetType()) {
+            'PSCustomObject' { $Reference.id }
+            'int' { $Reference }
+            'string' {
+                $matchedObject = switch ($Type) {
+                    'Company' { Get-HuduCompany -Name $Reference }
+                    'AssetLayout' { Get-HuduAssetLayout -Name $Reference }
+                }
+                if ($matchedObject.Count -eq 1) { $matchedObject.id }
+                else { Write-Warning "Unable to identify $Type '$Reference' or more than one result returned."; $null }
+            }
+            Default { Write-Warning "Unable to determine value of $Type parameter."; $null }
+        }
+        Write-Debug "Found $Type [$id] from [$($Reference.GetType().Name)]$($Reference | ConvertTo-Json -Depth 5 -Compress)"
+        return $id
+    }
+}
