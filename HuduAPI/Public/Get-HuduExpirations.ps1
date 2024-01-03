475849116b23b7df3186ec1db9dd8d65a00be844
adamburley <52085927+adamburley@users.noreply.github.com>
2024-01-03 11:02:23 -0500

    Rewrite

--- HuduAPI/Public/Get-HuduExpirations.ps1
+++ HuduAPI/Public/Get-HuduExpirations.ps1
@@ -1,55 +1,54 @@
 function Get-HuduExpirations {
-    <#
-    .SYNOPSIS
-    Get expirations for account
-
-    .DESCRIPTION
-    Calls Hudu API to retrieve expirations
-
-    .PARAMETER CompanyId
-    Filter expirations by company_id
-
-    .PARAMETER ExpirationType
-    Filter expirations by expiration type (undeclared, domain, ssl_certificate, warranty, asset_field, article_expiration)
-
-    .PARAMETER ResourceId
-    Filter logs by resource id. Must be coupled with resource_type
-
-    .PARAMETER ResourceType
-    Filter logs by resource type (Asset, AssetPassword, Company, Article, etc.). Must be coupled with resource_id
-
-    .EXAMPLE
-    Get-HuduExpirations -ExpirationType domain
-
-    #>
     [CmdletBinding()]
     Param (
         [Alias('company_id')]
-        [Int]$CompanyId = '',
+        [object]$Company,
 
-        [ValidateSet('undeclared', 'domain', 'ssl_certificate', 'warranty', 'asset_field', 'article_expiration')]
+        [ValidateSet('undeclared', 'domain', 'ssl_certificate', 'warranty', 'asset_field', 'article_expiration',IgnoreCase=$false,ErrorMessage='Expiration type names are case-sensitive. Valid values are undeclared, domain, ssl_certificate, warranty, asset_field, and article_expiration.')]
         [Alias('expiration_type')]
-        [String]$ExpirationType = '',
+        [string]$ExpirationType,
 
-        [Alias('resource_id')]
-        [Int]$ResourceId = '',
+        [parameter(ValueFromPipeline)]
+        [Alias('resource_id','ResourceId')]
+        [object]$Resource,
 
-        [Alias('resource_type')]
-        [String]$ResourceType = ''
+        [parameter()]
+        [Alias('resource_type','asset_type')]
+        [ValidateSet('Article','Asset','AssetPassword','Website',IgnoreCase=$false,ErrorMessage='Resource type names are case-sensitive. Valid values are Article, Asset, and Website.')]
+        [string]$ResourceType
+
+   
     )
-
-    $Params = @{}
-
-    if ($CompanyId) { $Params.company_id = $CompanyId }
-    if ($ExpirationType) { $Params.expiration_type = $ExpirationType }
-    if ($ResourceType) { $Params.resource_type = $ResourceType }
-    if ($ResourceId) { $Params.resource_id = $ResourceId }
-
-    $HuduRequest = @{
-        Method   = 'GET'
-        Resource = '/api/v1/expirations'
-        Params   = $Params
+    process {
+        if ($Resource){
+            if ($Resource.object_type -and $Resource.object_type -iin @( 'Article','Asset','AssetPassword','Website' )) { # Catch asset reference objects
+                $_resourceType = $Resource.object_type
+                $_resourceId = $Resource.id
+            } elseif ($ResourceType) {
+                if ($resId = $Resource | Find-ObjectIdByReference -Type $ResourceType -ErrorAction SilentlyContinue) {
+                    $_resourceType = $ResourceType
+                    $_resourceId = $resId
+                } else {
+                    Write-Error "Unable to identify resource type for '$($Resource | ConvertTo-Json -Depth 5 -Compress)' of type $ResourceType. Resource Type is mandatory if string or INT specified for resource." -ErrorAction Stop
+                }
+            } else {
+                Write-Error "Unable to identify resource type for '$($Resource | ConvertTo-Json -Depth 5 -Compress)' of type $ResourceType. Resource Type is mandatory if string or INT specified for resource." -ErrorAction Stop
+            }
+        }
+        $_companyId = $Company | Find-ObjectIdByReference -Type 'Company' -ErrorAction Stop
+        
+        $Params = @{}
+        if ($_companyId) { $Params.company_id = $_companyId }
+        if ($ExpirationType) { $Params.expiration_type = $ExpirationType }
+        if ($_resourceType) { $Params.resource_type = $_resourceType }
+        if ($_resourceId) { $Params.resource_id = $_resourceId }
+    
+        $HuduRequest = @{
+            Method   = 'GET'
+            Resource = '/api/v1/expirations'
+            Params   = $Params
+        }
+        Write-Verbose "Request parameters: $($Params | ConvertTo-Json -Depth 3 -Compress)"
+        Invoke-HuduRequestPaginated -HuduRequest $HuduRequest
     }
-
-    Invoke-HuduRequestPaginated -HuduRequest $HuduRequest
 }
