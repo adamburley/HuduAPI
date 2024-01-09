d38cea358ea51a43ae9a7e0ff72a9c2d1a6648e3
adamburley <52085927+adamburley@users.noreply.github.com>
2024-01-09 07:41:06 -0500

    Update Find-ObjectIdByReference.ps1

--- HuduAPI/Private/Find-ObjectIdByReference.ps1
+++ HuduAPI/Private/Find-ObjectIdByReference.ps1
@@ -1,3 +1,4 @@
+# TODO - issue with returning object where a presented ID integer is returned, not the object
 function Find-ObjectIdByReference {
     [CmdletBinding(PositionalBinding)]
     param(
@@ -6,8 +7,11 @@ function Find-ObjectIdByReference {
         [object]$Reference,
 
         [Parameter(Position = 0, Mandatory)]
-        [ValidateSet('Asset','Company', 'AssetLayout')]
-        [string]$Type
+        [ValidateSet('Article','Asset','Company', 'AssetLayout', 'Website')]
+        [string]$Type,
+
+        [Parameter()]
+        [switch]$ReturnObject
     )
     process {
         if ($null -eq $Reference) { return $null }
@@ -16,11 +20,15 @@ function Find-ObjectIdByReference {
             {$_ -iin @('Int32','Int64')} { $Reference }
             'string' {
                 $matchedObject = switch ($Type) {
+                    'Article' { Get-HuduArticle -Name $Reference }
                     'Company' { Get-HuduCompany -Name $Reference }
                     'AssetLayout' { Get-HuduAssetLayout -Name $Reference }
                     'Asset' { Get-HuduAsset -Name $Reference }
+                    'Website' { Get-HuduWebsite -Name $Reference }
                 }
-                if ($matchedObject.Count -eq 1) { $matchedObject.id }
+                if ($matchedObject.Count -eq 1) { 
+                    if ($ReturnObject) { $matchedObject } else { $matchedObject.id }
+                }
                 else { Write-Error "Unable to identify $Type '$Reference' or more than one result returned."; $null }
             }
             Default { Write-Error "Unable to determine value of $Type parameter."; $null }
