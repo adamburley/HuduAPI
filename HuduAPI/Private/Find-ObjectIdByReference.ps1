5e68577a0663fbb36d7c0c2bac4fa6f54f8833b2
adamburley <52085927+adamburley@users.noreply.github.com>
2024-01-11 16:07:35 -0500

    Add -ReturnObject parameter

--- HuduAPI/Private/Find-ObjectIdByReference.ps1
+++ HuduAPI/Private/Find-ObjectIdByReference.ps1
@@ -7,7 +7,7 @@ function Find-ObjectIdByReference {
         [object]$Reference,
 
         [Parameter(Position = 0, Mandatory)]
-        [ValidateSet('Article','Asset','Company', 'AssetLayout', 'Website')]
+        [ValidateSet('Article', 'Asset', 'Company', 'AssetLayout', 'Website')]
         [string]$Type,
 
         [Parameter()]
@@ -15,25 +15,25 @@ function Find-ObjectIdByReference {
     )
     process {
         if ($null -eq $Reference) { return $null }
-        $id = switch ($Reference.GetType().Name) {
-            'PSCustomObject' { $Reference.id }
-            {$_ -iin @('Int32','Int64')} { $Reference }
-            'string' {
-                $matchedObject = switch ($Type) {
-                    'Article' { Get-HuduArticle -Name $Reference }
-                    'Company' { Get-HuduCompany -Name $Reference }
-                    'AssetLayout' { Get-HuduAssetLayout -Name $Reference }
-                    'Asset' { Get-HuduAsset -Name $Reference }
-                    'Website' { Get-HuduWebsite -Name $Reference }
-                }
-                if ($matchedObject.Count -eq 1) { 
-                    if ($ReturnObject) { $matchedObject } else { $matchedObject.id }
-                }
-                else { Write-Error "Unable to identify $Type '$Reference' or more than one result returned."; $null }
+        if ($Reference.GetType().Name -eq 'PSCustomObject') { $result = $ReturnObject ? $Reference : $Reference.id }
+        elseif ($Reference.GetType().Name -iin @('Int32', 'Int64') -and -not $ReturnObject) { $result = $Reference }
+        else {
+            $splat = if ($Reference.GetType().Name -iin @('Int32', 'Int64')) { @{ Id = $Reference } } else {
+                @{ Name = $Reference }
             }
-            Default { Write-Error "Unable to determine value of $Type parameter."; $null }
+            $result = switch ($Type) {
+                'Article' { Get-HuduArticle @splat }
+                'Company' { Get-HuduCompany @splat }
+                'AssetLayout' { Get-HuduAssetLayout @splat }
+                'Asset' { Get-HuduAsset @splat }
+                'Website' { Get-HuduWebsite @splat }
+            }
+            if ($result.Count -eq 1) { 
+                $result = $ReturnObject ? $result : $result.id
+            }
+            else { Write-Error "Unable to identify $Type '$Reference' or more than one result returned."; $null }
         }
         Write-Debug "Found $Type [$id] from [$($Reference.GetType().Name)]$($Reference | ConvertTo-Json -Depth 5 -Compress)"
-        return $id
+        return $result
     }
 }
